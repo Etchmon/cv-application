18b5fbc4283346eba7db275b329e8875ab83e9ca
James Etchells <etch.rush@gmail.com>
2022-04-08 13:48:00 -0700

    Add handle functions for each input

--- src/components/Personal.js
+++ src/components/Personal.js
@@ -13,25 +13,52 @@ class Personal extends Component {
         };
     };
 
-    handleChange = (e) => {
+    onSubmit = (e) => {
+        e.preventDefault();
+        console.log(this.state);
+    }
+
+    handleNameChange = (e) => {
         this.setState({
             personal: {
-                [e.target.name]: e.target.value
+                name: e.target.value,
+                email: this.state.personal.email,
+                phone: this.state.personal.phone,
             }
         });
-        console.log(this.state)
+    };
+
+    handleEmailChange = (e) => {
+        this.setState({
+            personal: {
+                name: this.state.personal.name,
+                email: e.target.value,
+                phone: this.state.personal.phone,
+            }
+        });
+    };
+
+    handlePhoneChange = (e) => {
+        this.setState({
+            personal: {
+                name: this.state.personal.name,
+                email: this.state.personal.email,
+                phone: e.target.value,
+            }
+        });
     };
 
     render() {
+        const { personal } = this.state;
         return (
-            <form>
+            <form onSubmit={this.onSubmit}>
                 <legend>Personal Information</legend>
                 <label htmlFor="nameInput">Name</label>
-                <input onChange={this.handleChange} type="text" id="nameInput" name="name" />
+                <input onChange={this.handleNameChange} type="text" id="nameInput" value={personal.name} />
                 <label htmlFor="nameInput">Email</label>
-                <input onChange={this.handleChange} type="text" id="emailInput" name="email" />
+                <input onChange={this.handleEmailChange} type="text" id="emailInput" value={personal.email} />
                 <label htmlFor="nameInput">Phone</label>
-                <input onChange={this.handleChange} type="text" id="phoneInput" name="phone" />
+                <input onChange={this.handlePhoneChange} type="text" id="phoneInput" value={personal.phone} />
                 <button type="Submit">
                     Add
                 </button>
